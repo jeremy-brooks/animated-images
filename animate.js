72b0bdf4f68881444b54087cb0052c1525c46317
jeremy-brooks <jeremybrooks321@gmail.com>
2016-08-29 19:48:50 +0100

    added a selector for start/end year month and set default image to the first in the collection

--- animate.js
+++ animate.js
@@ -11,6 +11,12 @@ var AnimationController = function() {
         localScope.startImageIndex = 0;
         localScope.animationTimer = NaN;
         localScope.imageLocationBase = "data/";
+        localScope.startYear = "";
+        localScope.startMonth = "";
+        localScope.endYear = "";
+        localScope.endMonth = "";
+        localScope.years = [];
+        localScope.months = [];
     })(this);
 
     // set images
@@ -22,20 +28,37 @@ var AnimationController = function() {
             dataType: "text",
             success: function (data) {
                 localScope.images = data.split("\n");
+                localScope.years.length = 0;
+                localScope.months.length = 0;
                 if (localScope.images.length > 0) {
                     localScope.images.length = localScope.images.length - 1;
                 }
                 for (var imageNameIndex = 0, imageName = ""; imageName = localScope.images[imageNameIndex]; imageNameIndex++) {
                     localScope.images[imageNameIndex] = localScope.imageLocationBase + imageName;
-                    localScope.addOptionToStartImageSelector(imageName);
+                    localScope.addOptionToSelector(imageName, "setStartImageSelector");
+                    var year = imageName.substring(0,4);
+                    if (localScope.years.indexOf(year) == -1) {
+                        localScope.years.push(year);
+                        localScope.addOptionToSelector(year, "startYearSelector");
+                        localScope.addOptionToSelector(year, "endYearSelector");
+                    }
+                    var month = imageName.substring(4,6);
+                    if (localScope.months.indexOf(month) == -1) {
+                        localScope.months.push(month);
+                        localScope.addOptionToSelector(month, "startMonthSelector");
+                        localScope.addOptionToSelector(month, "endMonthSelector");
+                    }
+                    if (imageNameIndex == 0){
+                        document.getElementById("imageToAnimate").src = localScope.images[localScope.currentImageIndex];
+                    }
                 }
             }
         });
     };
 
-    this.addOptionToStartImageSelector = function (fileName) {
-        var newOption = new Option(fileName, fileName);
-        document.getElementById("setStartImageSelector").appendChild(newOption);
+    this.addOptionToSelector = function (option, selectorId) {
+        var newOption = new Option(option, option);
+        document.getElementById(selectorId).appendChild(newOption);
     };
     
     // pause animation
@@ -82,5 +105,21 @@ var AnimationController = function() {
         this.startImageIndex = this.images.indexOf(this.imageLocationBase + value);
         this.currentImageIndex = this.startImageIndex;
         this.startAnimation();
-    }
+    };
+
+    this.setStartYear = function (value) {
+        this.startYear = value;
+    };
+
+    this.setStartMonth = function (value) {
+        this.startMonth = value;
+    };
+
+    this.setEndYear = function (value) {
+        this.endYear = value;
+    };
+
+    this.setEndMonth = function (value) {
+        this.endMonth = value;
+    };
 };
