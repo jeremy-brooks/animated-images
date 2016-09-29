1dcee11aff75a9059936dbde054fa2e8e39e1044
jeremy-brooks <jeremybrooks321@gmail.com>
2016-09-29 18:57:35 +0100

    Created an image metadata service

--- src/data/imageService.js
+++ src/data/imageService.js
@@ -12,35 +12,6 @@ var ImageService = function (imagesModel) {
         getImagesByDateRange(startDate, endDate);
     });
 
-    (function () {
-        imageMetaData = [];
-        $.ajax({
-            type: "GET",
-            url: "data/dataIndex.txt",
-            dataType: "text",
-            success: function (data) {
-                var imageFileNames = data.split("\n");
-                if (imageFileNames.length > 0) {
-                    imageFileNames.length = imageFileNames.length - 1;
-                }
-                for (var imageNameIndex = 0, imageName = ""; imageName = imageFileNames[imageNameIndex]; imageNameIndex++) {
-                    var imageNameAsDate = moment(imageName.substring(0, 8), "YYYYMMDD");
-                    var image = {
-                        "uri": "data/" + imageName,
-                        "date": imageNameAsDate
-                    };
-                    imageMetaData.push(image);
-                }
-                dispatchEvent(new CustomEvent(ai.observable.DATE_RANGE_MAX_MIN_CHANGED, {
-                    "detail": {
-                        "min": imageMetaData[0].date,
-                        "max": imageMetaData[imageMetaData.length-1].date
-                    }
-                }));
-            }
-        });
-    })();
-
     function getImagesByDateRange(startDate, endDate){
         // get the first 10 images
         
