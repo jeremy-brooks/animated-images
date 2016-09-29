5e3f82490bbe535727defccd1a1a26808515b81e
jeremy-brooks <jeremybrooks321@gmail.com>
2016-09-29 21:00:53 +0100

    hooked up basic  image loading on date choice, bit rough

--- src/animate/animationController.js
+++ src/animate/animationController.js
@@ -1,9 +1,27 @@
 var AnimationController = function (view) {
     
-    var animationView = $(view);
+    this.animationView = $(view);
+    this.viewId = this.animationView.attr("id");
+    this.images = $("#" + this.viewId + " .item img");
 
-    addEventListener(ai.observable.IMAGE_SET_CHANGED, function (event) {
-        console.log(event.detail);
-    });
+    this.setImagesInCarousel = function (currentImages, nextImages) {
+        this.clearALlImages(this.images);
+        for (var imageIndex = 0, image; image = currentImages[imageIndex]; imageIndex++){
+            $(this.images[imageIndex]).attr("src", image.uri);
+        }
+    };
+
+    this.clearALlImages = function (images) {
+        for (var imageIndex = 0, image; image = images[imageIndex]; imageIndex++){
+            $(image).attr("src", "");
+        }
+    };
+
+    (function(scope){
+        scope.clearALlImages(scope.images);
+        addEventListener(ai.observable.IMAGE_SET_CHANGED, function (event) {
+            scope.setImagesInCarousel(event.detail.currentImages, event.detail.nextImages);
+        });
+    })(this);
     
 };
